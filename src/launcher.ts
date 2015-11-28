808e9f5ebe4d4bbf7c30af5ce02e1777c2ae134e
Ilya Verbitskiy <iverbitskiy@gmail.com>
2015-11-28 00:37:28 -0600

    Created terminal launcher

--- src/launcher.ts
+++ src/launcher.ts
@@ -1,20 +1,21 @@
 "use strict";
 
 import path = require("path");
+import cp = require("child_process");
 import vscode = require("vscode");
 
 export class Launcher {
-	private workspacePath: string = null;
-	private activeItemPath: string = null;
-    private commands: Array<ICommand> = [];
-    private startTerminal: ICommand = null;
+	private _workspacePath: string = null;
+	private _activeItemPath: string = null;
+    private _commands: Array<ICommand> = [];
+    private _startTerminal: ICommand = null;
 
 	constructor(textEditor: vscode.TextEditor = null) {
-		this.workspacePath = vscode.workspace.rootPath;
+		this._workspacePath = vscode.workspace.rootPath;
 		if (textEditor !== null) {
-			this.activeItemPath = path.dirname(textEditor.document.fileName);
-            if (this.activeItemPath === ".") {
-                this.activeItemPath = null;
+			this._activeItemPath = path.dirname(textEditor.document.fileName);
+            if (this._activeItemPath === ".") {
+                this._activeItemPath = null;
             }
 		}
 
@@ -22,22 +23,14 @@ export class Launcher {
 	}
 
 	public runTerminalInItemFolder() {
-		if (this.activeItemPath === null) {
-			return;
-		}
-
-		if (this.startTerminal !== null) {
-            this.startTerminal.run(this.activeItemPath);
+		if (this._startTerminal !== null) {
+            this._startTerminal.run(this._activeItemPath);
         }
 	}
 
     public runTerminalInWorkspaceFolder () {
-		if (this.workspacePath === null) {
-			return;
-		}
-
-		if (this.startTerminal !== null) {
-            this.startTerminal.run(this.workspacePath);
+		if (this._startTerminal !== null) {
+            this._startTerminal.run(this._workspacePath);
         }
 	}
 
@@ -60,15 +53,90 @@ export class Launcher {
     }
 
     protected initCommands() {
-        this.commands = [];
+        this._commands = [];
 
-        // TODO Load commands from VS Code configuration
+        // TODO Load commands from the configuration
 
-        // TODO Load commands from workspace configuration
+        // Load terminal command
+        this._startTerminal = new TerminalCommand(this._workspacePath, this._activeItemPath);
     }
 }
 
 interface ICommand {
     description: string;
     run(startIn: string): void;
+}
+
+class Command implements ICommand {
+    private _executable: string;
+    private _parameters: string;
+    private _description: string;
+    private _workspacePath: string;
+	private _activeItemPath: string ;
+
+    constructor(description: string,
+                executable: string,
+                parameters: string,
+                workspacePath: string,
+                activeItemPath: string) {
+        this._description = description;
+        this._executable = executable;
+        this._parameters = parameters;
+        this._workspacePath = workspacePath === null ? "" : workspacePath;
+        this._activeItemPath = activeItemPath === null ? "" : activeItemPath;
+    }
+
+    get description(): string {
+        return this._description;
+    }
+
+    run(startIn: string): void {
+        let parameters = this.applyTemplate(this._parameters);
+        startIn = this.applyTemplate(startIn);
+        let command = this._executable + " " + parameters;
+
+        let options: any = {};
+        if (startIn) {
+            options.cwd = startIn;
+        }
+
+        cp.exec(command, options);
+    }
+
+    protected applyTemplate(str: string) {
+        if (!str) {
+            return str;
+        }
+
+        str = str.replace(/%item%/ig, this._activeItemPath);
+        str = str.replace(/%workspace%/ig, this._workspacePath);
+        return str;
+    }
+}
+
+class TerminalCommand extends Command {
+    constructor(workspacePath: string,
+                activeItemPath: string) {
+        let config = vscode.workspace.getConfiguration("launcher.terminal");
+        let executable = config.get<string>("executable", "");
+        let parameters = config.get<string>("parameters", "");
+        if (executable === "") {
+            if (process.platform === "win32") {
+                executable = "cmd.exe";
+                parameters = "/c start /wait";
+            } else if (process.platform === "darwin") {
+                executable = "iTerm.sh";
+                parameters = "";
+            } else if (process.platform === "linux") {
+                executable = "xterm";
+                parameters = "";
+            } else {
+                // Use Windows command by default
+                executable = "cmd.exe";
+                parameters = "/c start /wait";
+            }
+        }
+
+        super("Terminal", executable, parameters, workspacePath, activeItemPath);
+    }
 }
